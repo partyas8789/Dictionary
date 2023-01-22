0ccefc5a6016e931b852272aa7fd2ca5549451f2
pratyas8789 <kpratyas@gmail.com>
2023-01-22 10:53:56 +0530

    dictionary

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const text=document.getElementById("text")
 const maincontainerlower=document.getElementById("maincontainerlower")
 
 async function search() {
+    maincontainerlower.innerHTML=""
     console.log("aaa");
     const response=await fetch(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${text.value}?key=0386bca9-7411-4e24-b36e-18f10f344072`)
     const data=await response.json()
